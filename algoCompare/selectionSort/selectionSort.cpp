bc6e8656d2791163c3b64b2c5cd0adb5f0a9dce7
kattbussen <olleclaeson@gmail.com>
2015-10-13 12:27:34 +0200

    framework for selectionSort

--- algoCompare/selectionSort/selectionSort.cpp
+++ algoCompare/selectionSort/selectionSort.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include "selectionSort.h"
 
-void selectionSort()
+std::chrono::high_resolution_clock::time_point start;
+std::chrono::high_resolution_clock::time_point stop;
+		
+int* SelectionSort::sort(int arr[], int size)
 {
-	std::cout << "selectionSort." << std::endl;
+	int sorted[size];
+
+	startTimer();
+	
+	stopTimer();
+	
+	std::copy(sorted, sorted+size, arr);
+	return arr;
+}
+		
+std::chrono::duration<double> SelectionSort::getTime()
+{
+	std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>> (stop - start);
+	return time_span;
+}
+
+void SelectionSort::startTimer()
+{
+	start = std::chrono::high_resolution_clock::now();
+}
+	
+void SelectionSort::stopTimer()
+{
+	stop = std::chrono::high_resolution_clock::now();
 }
--- algoCompare/selectionSort/selectionSort.h
+++ algoCompare/selectionSort/selectionSort.h
@@ -1,1 +1,15 @@
-void selectionSort();
+#include <limits.h>
+#include <chrono>
+#include <algorithm>
+#include "include/AbstractAlgorithm.h"
+
+class SelectionSort : SortAlgorithm
+{
+	public:
+		int* sort(int[], int);
+		std::chrono::duration<double> getTime();
+
+	private:
+		void startTimer();
+		void stopTimer();
+};
